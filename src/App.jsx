7e5a1944f43c8ad084355b1311ec61c13fd08e2e
Sooraj Devlee <soorajdev012@gmail.com>
2024-07-03 12:14:00 +0530

    Add Loader in current tab

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Suspense, useState, lazy } from "react";
 import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Loader from "./components/Loader/Loader";
 // import Batch from "./components/Batch/Batch";
 import ModuleCard from "./components/Module/ModuleCard";
 import TestModule from "./components/TestModule/TestModule";
@@ -23,7 +24,7 @@ function App() {
 
         <div className="flex flex-col w-full gap-2 md:flex md:flex-row md:justify-between px-6 py-6 bg-slate-100">
           <Sidebar />
-          <Suspense fallback={<h1>Loding...</h1>}>
+          <Suspense fallback={<Loader />}>
             <Routes>
               <Route path="" element={<ModuleCard />}></Route>
               <Route path="module" element={<ModuleCard />}></Route>
