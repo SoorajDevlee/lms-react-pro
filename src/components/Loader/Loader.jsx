ba56819ce9d047cd1a36a75c22f75ef0ba5efff5
Sooraj Devlee <soorajdev012@gmail.com>
2024-07-03 12:33:06 +0530

    Add new Linear color Loader in current tab

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress from "@mui/material/LinearProgress";
+import { Stack } from "@mui/material";
 
 export default function Loader() {
-  return <CircularProgress disableShrink />;
+  return (
+    //   <CircularProgress disableShrink />
+    <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+      <LinearProgress color="secondary" />
+      <LinearProgress color="success" />
+      <LinearProgress color="inherit" />
+    </Stack>
+  );
 }
